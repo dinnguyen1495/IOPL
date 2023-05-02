b82124ddd7fcfb579be272f74add9723c92e0ebf
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2023-05-02 09:58:44 +0200

    Restore deleted line by mistake

--- static/ioplibrary/js/book_list.js
+++ static/ioplibrary/js/book_list.js
@@ -1,6 +1,7 @@
 function update_book_list(data) {
-    $("#search-result-notify").html("<p id=\"search-result-text\">Found <b><u>" + data["result_number"] + " books</u></b> in the library</p>")
+    $("#search-result-notify").html("<p id=\"search-result-text\">Found <b><u>" + data["result_number"] + " books</u></b> in the library</p>");
 
+    let filtered_list = ""
     for (let i = 0; i < data["books"].length; i++) {
         filtered_list +=
             "<div class=\"book-container\">\n" +
@@ -24,6 +25,7 @@ function update_book_list(data) {
             "    </div>\n" +
             "</div>";
     }
+
     $("#book-list").html(
         filtered_list
     );
