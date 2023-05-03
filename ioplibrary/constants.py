12e35e9d736990188732f4ec1710e401e1426231
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2023-05-03 16:38:13 +0200

    Update .gitignore

--- ioplibrary/constants.py
+++ ioplibrary/constants.py
@@ -2,3 +2,14 @@ COVERS_DIR = "covers"
 UNAVAILABLE_COVER_URL = f"{COVERS_DIR}/unavailable.jpg"
 EXCLUSION_SET = {"buy", "book", "amazon"}
 SKIP_SET = {"download", "free"}
+
+# Custom google search engine for google images
+# Limit (Stand 04.2023): 100 queries/day
+# GG_CUSTOM_SEARCH_API = "AIzaSyCX_uzDfSAr8kzU-gGsYaH_qGlac8p5RrA"
+# GG_CUSTOM_SEARCH_ENGINE_ID = "c015344985db54149"
+
+# GG_CUSTOM_SEARCH_API = "AIzaSyCVv8rfRjbXO5AsvhYYK7JohDB1vmTteJI"
+# GG_CUSTOM_SEARCH_ENGINE_ID = "61e8182e63c5b4ede"
+
+GG_CUSTOM_SEARCH_API = "AIzaSyBezOK25VzHBmN-flTXQGRpHCaoVcKbuaU"
+GG_CUSTOM_SEARCH_ENGINE_ID = "d0486d0dd546c4d6f"
