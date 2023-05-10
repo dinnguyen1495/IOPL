064877f2254ad02b2a253b45833348c76057175d
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2023-05-10 14:07:38 +0200

    Switch to asynchronize websocket

--- ioplibrary/consumers.py
+++ ioplibrary/consumers.py
@@ -1,22 +1,22 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class BookConsumer(WebsocketConsumer):
-    def connect(self):
-        async_to_sync(self.channel_layer.group_add)('update_book', self.channel_name)
-        self.accept()
+class BookConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add('update_book', self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)('update_book', self.channel_name)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard('update_book', self.channel_name)
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         print(f"Received: {text_data_json}")
 
-    def book_change(self, event):
-        self.send(text_data=json.dumps({"type": event["type"], "book_id": event["instance"]}))
+    async def book_change(self, event):
+        await self.send(text_data=json.dumps({"type": event["type"], "book_id": event["instance"]}))
 
-    def book_delete(self, event):
-        self.send(text_data=json.dumps({"type": event["type"], "book_id": event["instance"]}))
+    async def book_delete(self, event):
+        await self.send(text_data=json.dumps({"type": event["type"], "book_id": event["instance"]}))
